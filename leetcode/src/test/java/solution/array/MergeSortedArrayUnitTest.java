a891bc0d7ffe4676c15a184ac75a7d9b8f11eacf
cameliabroboana <cameliabroboana20@gmail.com>
2024-12-27 20:37:48 +0200

    removed unnecessary import

--- leetcode/src/test/java/solution/array/MergeSortedArrayUnitTest.java
+++ leetcode/src/test/java/solution/array/MergeSortedArrayUnitTest.java
@@ -7,7 +7,6 @@ import java.util.List;
 
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.testng.Assert.*;
 
 public class MergeSortedArrayUnitTest {
 
